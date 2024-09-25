35cc73647e290907ed07995f636e345a8ec91c46
Katie Herlingshaw <katieh@unis.no>
2024-09-25 22:40:40 +0200

    Delete docs/conf.py

--- docs/conf.py
+++
@@ -1,4 +0,0 @@
-html_theme = "sphinx_material"
-extensions = ["sphinx.ext.imgmath, "sphinx_material""]
-html_theme_options = {"nav_title": "My Project", "color_primary": "indigo", "color_accent": "pink"}
-master_doc = "index"
